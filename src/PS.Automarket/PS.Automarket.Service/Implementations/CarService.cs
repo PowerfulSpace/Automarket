642caf5f43486f13de33cace2ffd4e20e0d28942
Aliaksandr Kudrautsau <sany199574@gmail.com>
2022-08-20 08:55:58 +0300

    Добавлен новый слой Servic

---
+++ src/PS.Automarket/PS.Automarket.Service/Implementations/CarService.cs
@@ -0,0 +1,13 @@
+﻿using PS.Automarket.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Automarket.Service.Implementations
+{
+    public class CarService: ICarService
+    {
+    }
+}
